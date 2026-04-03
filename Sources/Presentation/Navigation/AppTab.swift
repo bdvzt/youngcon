545b34fc18f5a8761d7452cc24f4bb45d705e604
Margarita Yaganova <m.yaganova@bi.zone>
2026-04-03 15:25:35 +0300

    26: NavBar + LoginScreen

--- Sources/Presentation/Navigation/AppTab.swift
+++ Sources/Presentation/Navigation/AppTab.swift
@@ -5,25 +5,34 @@ enum AppTab: CaseIterable {
 
     var label: String {
         switch self {
-        case .map:      return "Карта"
-        case .schedule: return "Расписание"
-        case .badge:    return "Бейдж"
+        case .map:
+            "Карта"
+        case .schedule:
+            "Расписание"
+        case .badge:
+            "Бейдж"
         }
     }
 
     var icon: String {
         switch self {
-        case .map:      return "map"
-        case .schedule: return "calendar"
-        case .badge:    return "person.crop.circle"
+        case .map:
+            "map"
+        case .schedule:
+            "calendar"
+        case .badge:
+            "person.crop.circle"
         }
     }
 
     var pageColor: Color {
         switch self {
-        case .map:      return Color("TabMapColor")
-        case .schedule: return Color("TabScheduleColor")
-        case .badge:    return Color("TabBadgeColor")
+        case .map:
+            Color("TabMapColor")
+        case .schedule:
+            Color("TabScheduleColor")
+        case .badge:
+            Color("TabBadgeColor")
         }
     }
 }
