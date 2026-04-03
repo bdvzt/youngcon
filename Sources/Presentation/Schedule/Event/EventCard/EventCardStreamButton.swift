aee15eeb30fd3e2fa81bb80c571d9c69d4a34747
zed932 <mescheryakovsrg@gmail.com>
2026-04-03 16:06:45 +0300

    Fix view

--- Sources/Presentation/Schedule/Event/EventCard/EventCardStreamButton.swift
+++ Sources/Presentation/Schedule/Event/EventCard/EventCardStreamButton.swift
@@ -7,7 +7,7 @@ struct EventCardStreamButton: View {
             Rectangle()
                 .frame(width: 40, height: 40)
                 .foregroundColor(Color("AccentYellow"))
-                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .clipShape(RoundedRectangle(cornerRadius: 14))
             Image(systemName: "play.fill")
                 .resizable()
                 .frame(width: 14, height: 14)
