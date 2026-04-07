16117631e3510fbc1bf7effa4481bb39900203c8
Margarita Yaganova <m.yaganova@bi.zone>
2026-04-07 16:13:34 +0300

    59: fix reviews comments 4

--- Sources/Data/Network/Repository/Auth/DTO/AccessTokenDTO.swift
+++ Sources/Data/Network/Repository/Auth/DTO/AccessTokenDTO.swift
@@ -1,17 +1,3 @@
 struct AccessTokenDTO: Decodable {
     let token: String
-
-    private enum CodingKeys: String, CodingKey {
-        case token
-        case accessToken
-        case access_token
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        token = try container.decodeIfPresent(String.self, forKey: .token)
-            ?? container.decodeIfPresent(String.self, forKey: .accessToken)
-            ?? container.decodeIfPresent(String.self, forKey: .access_token)
-            ?? ""
-    }
 }
